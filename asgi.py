b9dc573f296dfac0653f53355efcc35d6de695e3
Iljin Teemu Sakari <Teemu.iljin@gmail.com>
2025-09-01 07:33:48 +0300

    Clean up repository: remove duplicate files, __pycache__, .DS_Store, and optimize structure for Railway deployment

--- asgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.asgi import get_asgi_application
-
-# Set default settings module for ASGI application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brainbank.settings')
-
-application = get_asgi_application()
